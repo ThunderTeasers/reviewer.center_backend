a2e6ab63a432deec5e358bf5eedc6cedd62c2f57
ThunderTeasers <thunderteasers@yandex.ru>
2025-02-12 02:51:20 +0300

    Добавлен контроллер компании

---
+++ src/controller/CompanyController.ts
@@ -0,0 +1,75 @@
+/**
+ * Контроллер компании
+ * =============
+ *
+ * @author Max Erokhin
+ */
+
+import Controller from './Controller';
+import Company from '../model/Company';
+import type Database from '../database/Database';
+
+class CompanyController extends Controller {
+  constructor(database: Database) {
+    super(database);
+  }
+
+  /**
+   * Получение одной компании
+   *
+   * @param {string} link Идентификатор компании
+   * @returns {Promise<Company>} Обещание, которое будет выполнено, когда будет получен объект компании
+   */
+  public async getOne(link: string): Promise<Company> {
+    const query = `
+      SELECT 
+        id,
+        title,
+        created_at,
+        updated_at
+      FROM company
+      WHERE link = ?
+      LIMIT 1
+    `;
+
+    const rows: Company[] = (await this._database.query(query, [
+      link,
+    ])) as Company[];
+
+    return rows[0];
+  }
+
+  /**
+   * Получение списка компаний
+   *
+   * @param {number} limit - Количество компаний, которое нужно получить
+   * @param {number} offset - Сдвиг начала списка
+   * @returns {Promise<Company[]>} - Обещание, которое будет выполнено, когда
+   *                                  будет получен список объектов компаний
+   */
+  public async getAll(
+    limit: number = 10,
+    offset: number = 0
+  ): Promise<Company[]> {
+    const query = `
+      SELECT 
+        id,
+        title,
+        link,
+        created_at,
+        updated_at
+      FROM company
+      LIMIT ?
+      OFFSET ?
+    `;
+
+    const rows: Company[] = (await this._database.query(query, [
+      limit,
+      offset,
+    ])) as Company[];
+
+    return rows;
+  }
+}
+
+export default CompanyController;
