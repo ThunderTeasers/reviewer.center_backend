fb01f20e0aa8980387220949b1007312c0cc1810
ThunderTeasers <thunderteasers@yandex.ru>
2025-02-19 03:24:28 +0300

    Добавлен путь страницы компании

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import Router from './router/Router';
 import Database from './database/Database';
 import CategoryController from './controller/CategoryController';
 import CompanyController from './controller/CompanyController';
+import ReviewController from './controller/ReviewController';
 import type Category from './model/Category';
 import type Company from './model/Company';
+import type Review from './model/Review';
 
 const database: Database = new Database();
 
@@ -29,6 +31,22 @@ new Router()
   })
   .get('/companies', async ({ query }: any) => {
     return await new CompanyController(database).getAll(query.get('category'));
+  })
+  .get('/companies/:link', async ({ params }: any) => {
+    const company: Company = await new CompanyController(database).getOne(
+      params.link
+    );
+
+    const reviews: Review[] = await new ReviewController(database).getAll(
+      company.id,
+      10,
+      0
+    );
+
+    return {
+      company,
+      reviews,
+    };
   })
   .get('/about', () => 'about')
   .get('/faq', () => 'faq')
